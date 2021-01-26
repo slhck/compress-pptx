f82e6e5a344df319220a7030d9634504d495f3b3
Werner Robitza <werner.robitza@gmail.com>
2021-01-26 11:17:27 +0100

    add script and README

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
 # compress-pptx
-Compress a PPTX file
+
+Compress a PPTX file, converting all PNG/TIFF images to lossy JPEGs.
+
+## What it does
+
+When copy-pasting images to PowerPoint presentations, these sometimes get inserted as lossless versions, blowing up the size of the presentation.
+
+This script takes all PNG or TIFF images part of the presentation which are larger than a given threshold (1 MiB by default), converts them to a lossy JPEG variant, and creates a new PPTX file.
+
+:warning: This will not work for PNG images containing transparency!
+
+## Requirements
+
+This script runs under *nix systems and presumably also WSL.
+
+- Bash
+- Perl
+- ImageMagick's `convert`
+- `zip` and `unzip`
+
+## Usage
+
+Call the script and point it to a PPTX file. It'll compress the images and output another compressed file next to it.
+
+For more options, see the `-h` output:
+
+```
+./compress_pptx.sh [-s SIZE] [-q QUALITY] [-o OUTPUT] <input>
+
+  -s SIZE     Minimum size in MiB for images to be compressed (default: 1)
+  -q QUALITY  JPEG quality from 0-100 (default: 85)
+  -o OUTPUT   Output file (default: input file with '-compressed.pptx' suffix)
+```
+
+## Version history
+
+- v0.1: Initial release
+
+## License
+
+MIT License
+
+Copyright (c) 2021 Werner Robitza
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ compress_pptx.sh
@@ -0,0 +1,105 @@
+#!/usr/bin/env bash
+#
+# PPTX compression script.
+#
+# Compresses all images within a PPTX input that are too large.
+#
+# Options are available to set JPEG quality and size threshold.
+# See `-h` for more.
+#
+# Version: v0.1
+# Copyright (c) 2021, Werner Robitza <werner.robitza@gmail.com>
+# License: MIT
+
+set -e
+set -o pipefail
+
+# Check necessary dependencies
+for cmd in perl convert zip unzip; do
+  command -v "$cmd" >/dev/null 2>&1 || { echo >&2 "$cmd not found in \$PATH. Please install it!"; exit 1; }
+done
+
+usage() {
+  echo "Usage:"
+  echo
+  echo "$0 [-s SIZE] [-q QUALITY] [-o OUTPUT] <input>"
+  echo
+  echo "  -s SIZE     Minimum size in MiB for images to be compressed (default: 1)"
+  echo "  -q QUALITY  JPEG quality from 0-100 (default: 85)"
+  echo "  -o OUTPUT   Output file (default: input file with '-compressed.pptx' suffix)"
+  echo
+  echo "compress-pptx v0.1"
+  echo "Copyright (c) 2021, Werner Robitza"
+  echo "License: MIT"
+  exit 1
+}
+
+output=
+size="1" # MB above which to compress a media (PNG, TIFF) file
+quality="85" # JPEG quality
+
+while getopts "s:q:o:" flag; do
+case "$flag" in
+    s) size=$OPTARG;;
+    q) quality=$OPTARG;;
+    o) output=$OPTARG;;
+    *) usage;;
+esac
+done
+
+input=${*:$OPTIND:1}
+filename="$(basename "$input")"
+srcDir=$(realpath "$(dirname "$input")")
+
+# if no default was set
+if [[ -z "$output" ]]; then
+  output="${srcDir}/${filename%%.pptx}-compressed.pptx"
+fi
+
+# Temporary folder
+tmpDir="$(mktemp -d -t tmp.XXXXXXXXXX)"
+__cleanup() {
+  rm -rf "$tmpDir"
+}
+
+# =============================================================
+
+echo "Unzipping file ..."
+unzip -q "$input" -d "$tmpDir"
+
+echo "Finding files larger than ${size} MiB ..."
+# TODO parallelize this if possible?
+find "${tmpDir}/ppt/media" \
+  -type f \
+  \( -iname '*.png' -o -iname '*.tiff' \) \
+  -size +"${size}M" \
+  -print0 | while read -r -d $'\0' file; do
+  filename=$(basename -- "$file")
+  dir="$(dirname -- "$file")"
+  target="compressed_${filename%.*}.jpg"
+
+  echo "Compressing ${filename} ($(du -h "$file" | cut -f -1 | tr -d ' '))"
+  convert \
+    -quality "${quality}" \
+    "${file}" "${dir}/${target}"
+  rm -- "$file"
+
+  # replace filenames in XML
+  # FIXME: this might be a bit hacky
+  find "${tmpDir}/ppt/slides/_rels/" \
+    -iname '*.rels' \
+    -exec perl -pi -e "s/${filename}/${target}/g" {} \+
+done
+
+pushd "$tmpDir" > /dev/null || exit
+echo "Writing output to $output ..."
+zip -q -r "$output" .
+popd  > /dev/null || exit
+
+echo "Done."
+
+echo "Input size:  $(du -h "$input" | cut -f -1)"
+echo "Output size: $(du -h "$output" | cut -f -1)"
+__cleanup
+
+trap __cleanup EXIT
